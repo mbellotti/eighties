3d5723b993b03d1230da6b81683fc956bb617a2c
Marianne Bellotti <mbellotti@mariannes-air.home>
2014-07-07 13:22:37 -0400

    fiddling with templates

--- content-page.php
+++ content-page.php
@@ -9,11 +9,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+	<!--header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
+	<div class="entry-content" style="max-width:1200px !important;">
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
