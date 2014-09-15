01e07a9c5f9818ab17e442332036e9fe23a5c272
Marianne Bellotti <mbellotti@mariannes-air.home>
2014-09-15 13:14:08 -0400

    cat id

--- index.php
+++ index.php
@@ -17,8 +17,10 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<?php $args = array(
-		       'cat' => '-hide',
+		<?php 
+			$category_id = get_cat_ID('hide');
+		$args = array(
+		       'cat' => '-'.$category_id,
 		       'posts_per_page' => 10,
 		       'order' => 'DESC'
 		);
